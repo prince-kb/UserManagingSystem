f6edd42e32be39e1f1c6eb5732d537aa76974234
Prince <princejaj67@gmail.com>
2024-04-10 00:14:09 +0530

    Finishing add User function

--- usersystem/src/App.js
+++ usersystem/src/App.js
@@ -1,96 +1,253 @@
-import { useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import UserItem from "./components/UserItem";
-import la from './assets/svg/leftarrow.svg'
-import ra from './assets/svg/rightarrow.svg'
-import spinner from './assets/svg/spinner.svg'
-
+import la from "./assets/svg/leftarrow.svg";
+import ra from "./assets/svg/rightarrow.svg";
+import spinner from "./assets/svg/spinner.svg";
 
 function App() {
-  const [fixedUsers,setFixedUsers]= useState([]);
-  const [isLoading,setLoading]=useState(true);
-  const [suser,setSuser]=useState()
-  const [list,setList]=useState([]);
-  const [l,setL] = useState([]);
-  const [i,setI]=useState(0);
+  const [fixedUsers, setFixedUsers] = useState([]);
+  const [isLoading, setLoading] = useState(true);
+  const [suser, setSuser] = useState();
+  const [list, setList] = useState([]);
+  const [n, setN] = useState({});
+  const [h, setH] = useState("hidden");
+  const [l, setL] = useState([]);
+  const [i, setI] = useState(0);
 
 
-  const p = async()=>{
+  const p = async () => {
     const response = await fetch(`${process.env.REACT_APP_HOST}/api/users`, {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const n = await response.json();
+    return n;
+  };
+
+  useEffect(() => {
+    p()
+      .then((da) => {
+        setFixedUsers(da.users);
+      })
+      .catch((err) => {
+        console.log("Error occured! Try refreshing");
+      });
+  }, []);
+
+  useEffect(() => {
+    if (fixedUsers.length > 0) {
+      setList(fixedUsers);
+      setLoading(false);
     }
-  });
-  const n = await response.json();
-  return n;
-}
+  }, [fixedUsers]);
 
-useEffect(()=>{
-  p().then((da)=>{
-    setFixedUsers(da.users)})
-    .catch((err)=>{console.log("Error occured! Try refreshing")})
-},[])
+  useEffect(() => {
+    setL(
+      list.slice(20 * i, 20 * i + 20 < list.length ? 20 * i + 20 : list.length)
+    );
+  }, [list, i]);
 
-useEffect(()=>{
-  if(fixedUsers.length >0 ){
-    setList(fixedUsers)
-    setLoading(false);
-  }
-},[fixedUsers])
- 
-useEffect(()=>{
-  setL(list.slice(20*i,20*i+20<list.length ? 20*i+20 : list.length));
-},[list,i])
-
-
-  const onChange=(e)=>{
+  const onChange = (e) => {
     // console.log(e)
-    setSuser(e.target.value)
+    setSuser(e.target.value);
     setI(0);
-    let z = e.target.value.toLowerCase()
-    setList(list.filter((val)=>{
-      return val.first_name.slice(0,z.length).toLowerCase()===z || val.last_name.slice(0,z.length).toLowerCase()===z
-    }))
-  }
+    let z = e.target.value.toLowerCase();
+    setList(
+      list.filter((val) => {
+        return (
+          val.first_name.slice(0, z.length).toLowerCase() === z ||
+          val.last_name.slice(0, z.length).toLowerCase() === z
+        );
+      })
+    );
+  };
 
-    const submit=(e)=>{
+  const submit = (e) => {
+    e.preventDefault();
+    setI(0);
+    setL(
+      list.slice(20 * i, 20 * i + 20 < list.length ? 20 * i + 20 : list.length)
+    );
+    let z = suser.toLowerCase;
+    setList(
+      list.filter((val) => {
+        return (
+          val.first_name.slice(0, z.length).toLowerCase() === z ||
+          val.last_name.slice(0, z.length).toLowerCase() === z
+        );
+      })
+    );
+  };
+
+  const onChangeee = (e) => {
+    console.log(e);
+  };
+
+  const addUs =(e)=>{
     e.preventDefault()
-    setI(0);
-    setL(list.slice(20*i,20*i+20<list.length ? 20*i+20 : list.length));
-    let z = suser.toLowerCase
-    setList(list.filter((val)=>{
-      return val.first_name.slice(0,z.length).toLowerCase()===z|| val.last_name.slice(0,z.length).toLowerCase()===z
-    }))
-  }
-
-    // setList(fixedUsers)}
+    console.log("Submitted");
+    setN({});
+}
 
   return (
     <div className=" text-center flex-col">
       {/* <div><div className="tenor-gif-embed" data-postid="23918898" data-share-method="host" data-aspect-ratio="1" data-width="100%"><a href="https://tenor.com/view/loading-gif-gif-23918898">Loading Gif Sticker</a>from <a href="https://tenor.com/search/loading+gif-stickers">Loading Gif Stickers</a></div> <script type="text/javascript" async src="https://tenor.com/embed.js"></script></div> */}
-      <h1 className="h1 container bg-emerald-300">Welcome</h1>
-      <div className="mx-2 flex items-center justify-center gap-[2vw]">
-      <input type="text" onBlur={()=>setList(fixedUsers)} className="form-control" id="s" placeholder="Search user by name" name="s" value={suser} onChange={onChange} minLength={1}/>
-        <button type="button" className="btn btn-primary" onClick={submit}>Search</button>
+      <h1 className="h1 container my-[3vh]">User Management System</h1>
+      <div className="mx-2 flex items-center justify-center gap-[2vw] my-[3vh]">
+        <input
+          type="text"
+          onBlur={() => setList(fixedUsers)}
+          className="form-control ml-[10vw]"
+          id="s"
+          placeholder="Search user by name"
+          name="s"
+          value={suser}
+          onChange={onChange}
+          minLength={1}
+        />
+        <button type="button" className="btn btn-primary mr-[10vw]" onClick={submit}>
+          Search
+        </button>
+      </div>
+
+      {/* Add User */}
+      <button className={`btn btn-outline-primary m-2 ${h==='hidden' ? '' : 'hidden'}`} onClick={() => {h === "hidden" ? setH("") : setH("hidden")}}>Add a user</button>
+      <div className={`m-4 ${h}`}>
+        <div className="flex justify-center">
+        <h2 className="h2 container " >Add user details</h2>
+        <h2 className="h2 mx-[10vw] cursor-pointer" onClick={()=>{h==="hidden" ? setH("") : setH("hidden")}}>X</h2>
         </div>
-
-      <div className="card card-body" style={{width: "100vw"}}>
-            <div className="row">
-                <h2 className="h2 d-flex justify-content-center"> <b>All Users</b> </h2>
-                {isLoading && <div className="flex justify-center"><img src={spinner} alt="Loading....." /></div>}
-                {l.length>0 ? l.map((user,index) =>(
-                  <UserItem key={index} user = {user}/>)
-                 ) : <h2 className="h2">No Users available</h2>}
-              </div>    
-            </div>
-
-            <div className="w-full flex justify-center ">
+        <form className="container mb-3">
+          <div className="form-floating mb-3">
+            <input
+              type="text"
+              className="form-control"
+              id="eid"
+              name="eid"
+              value={n.eid}
+              onChange={onChangeee}
+            />
+            <label htmlFor="eid">ID of the user</label>
+          </div>
+          <div className="form-floating mb-3">
+            <input
+              type="text"
+              className="form-control"
+              id="efname"
+              name="efname"
+              value={n.efname}
+              onChange={onChangeee}
+            />
+            <label htmlFor="efname">First Name</label>
+          </div>
+          <div className="form-floating mb-3">
+            <input
+              type="text"
+              className="form-control"
+              id="elname"
+              name="elname"
+              value={n.lname}
+              onChange={onChangeee}
+            />
+            <label htmlFor="elname">Last Name</label>
+          </div>
+          <div className="form-floating mb-3">
+            <input
+              type="text"
+              className="form-control"
+              id="egender"
+              name="egender"
+              value={n.egender}
+              onChange={onChangeee}
+            />
+            <label htmlFor="egender">Gender of user</label>
+          </div>
+          <div className="form-floating mb-3">
+            <input
+              type="text"
+              className="form-control"
+              id="eavatar"
+              name="eavatar"
+              value={n.eavatar}
+              onChange={onChangeee}
+            />
+            <label htmlFor="eavatar">Avatar Link</label>
+          </div>
+          <div className="form-floating mb-3">
+            <input
+              type="text"
+              className="form-control"
+              id="edomain"
+              name="edomain"
+              value={n.edomain}
+              onChange={onChangeee}
+            />
+            <label htmlFor="edomain">Domain</label>
+          </div>
+          <div class="flex items-center justify-around mb-3">
             <div className="flex items-center">
-            <button><img src={la} alt="" className="h-[4vh]" onClick={()=>{i > 0 ? setI(i-1) : setI(i)}}/></button>
-            <h3 className="h3 mx-3">Page : {i+1}</h3>
-            <button><img src={ra} alt="" className="h-[4vh]" onClick={()=>{(list.length/20)>i+1 ? setI(i+1) : setI(i)}}/></button>
+            <input
+              type="checkbox"
+              name="avail"
+              id="avail"
+              className="h-[3vh] w-[3vh] mx-3"
+            />
+            <label for="avail" className=""><strong>Availability</strong></label>
             </div>
+            <button type="submit"  className="btn btn-primary" onClick={addUs}>Add Note</button>
+
+          </div>
+
+        </form>
+      </div>
+
+      <div className="card card-body" style={{ width: "100vw" }}>
+        <div className="row">
+          <h2 className="h2 d-flex justify-content-center">
+            {" "}
+            <b>All Users</b>{" "}
+          </h2>
+
+          {isLoading && (
+            <div className="flex justify-center">
+              <img src={spinner} alt="Loading....." />
             </div>
+          )}
+          {l.length > 0 ? (
+            l.map((user, index) => <UserItem key={index} user={user} />)
+          ) : (
+            <h2 className="h2">No Users available</h2>
+          )}
+        </div>
+      </div>
+
+      <div className="w-full flex justify-center ">
+        <div className="flex items-center">
+          <button>
+            <img
+              src={la}
+              alt=""
+              className="h-[4vh]"
+              onClick={() => {
+                i > 0 ? setI(i - 1) : setI(i);
+              }}
+            />
+          </button>
+          <h3 className="h3 mx-3">Page : {i + 1}</h3>
+          <button>
+            <img
+              src={ra}
+              alt=""
+              className="h-[4vh]"
+              onClick={() => {
+                list.length / 20 > i + 1 ? setI(i + 1) : setI(i);
+              }}
+            />
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
--- usersystem/src/components/UserItem.js
+++ usersystem/src/components/UserItem.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useState,useRef} from 'react'
 import add from '../assets/svg/add.svg'
 import del from '../assets/svg/delete.svg'
 import edit from '../assets/svg/edit.svg'
@@ -7,9 +7,86 @@ import Tooltip from '@mui/material/Tooltip';
 
 
 const UserItem = (props) => {
+  const [n,setN]=useState({});
+  const ref = useRef(null)
+  const refClose = useRef(null)
     const idd=props.user;
+
+
+    const onChangeee=()=>{
+
+    }
+    const submittt=()=>{
+      setN({})
+    }
+    const edituser=(e)=>{
+      ref.current.click();
+    }
+
     return (
         <div className="col-md-3 my-3 mx-auto">
+
+
+
+        {/*Modale */}
+        
+        <div className="d-flex justify-content-center my-3">
+          <button type="button" className="btn btn-primary d-none" data-bs-toggle="modal" ref={ref} data-bs-target="#staticBackdrop">
+            Edit Note
+          </button>
+          <div className="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabIndex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+            <div className="modal-dialog">
+              <div className="modal-content">
+                <div className="modal-header">
+                  <h1 className="modal-title fs-5" id="staticBackdropLabel">Add User</h1>
+                  <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+
+                <div className="modal-body">
+                <h5 className="h5 container">Edit the user data</h5>
+                  <form className="container mb-3">
+
+                  <div className="form-floating mb-3">
+                    <input type="text" className="form-control" id="eid" name="eid" value={n.eid} onChange={onChangeee}/>
+                    <label htmlFor="eid">ID of the user</label>
+                  </div>
+                  <div className="form-floating mb-3">
+                    <input type="text" className="form-control" id="efname" name="efname" value={n.efname} onChange={onChangeee}/>
+                    <label htmlFor="efname">First Name</label>
+                  </div>
+                  <div className="form-floating mb-3">
+                    <input type="text" className="form-control" id="elname" name="elname" value={n.lname} onChange={onChangeee}/>
+                    <label htmlFor="elname">Last Name</label>
+                  </div>
+                  <div className="form-floating mb-3">
+                    <input type="text" className="form-control" id="egender" name="egender" value={n.egender} onChange={onChangeee}/>
+                    <label htmlFor="egender">Gender of user</label>
+                  </div>
+                  <div className="form-floating mb-3">
+                    <input type="text" className="form-control" id="eavatar" name="eavatar" value={n.eavatar} onChange={onChangeee}/>
+                    <label htmlFor="eavatar">Avatar Link</label>
+                  </div>
+                  <div className="form-floating mb-3">
+                    <input type="text" className="form-control" id="edomain" name="edomain" value={n.edomain} onChange={onChangeee}/>
+                    <label htmlFor="edomain">Domain</label>
+                  </div>
+                  
+
+                  </form>
+                </div>
+
+                <div className="modal-footer">
+                  <button type="button" ref={refClose} className="btn btn-danger" data-bs-dismiss="modal">X</button>
+                  <button type="submit" className="btn btn-primary" onClick={submittt}>Edit Note</button>
+                </div>
+              </div>
+            </div>
+          </div>
+          </div>
+
+
+
+
           <div className={`card m-auto ${window.innerWidth < 768 ? 'flex-row' : '' } `} >
             <div className='my-1'>
                 <img src={idd.avatar} alt="" className='mx-auto h-[10vh] w-[10vh]'/>
@@ -18,7 +95,7 @@ const UserItem = (props) => {
                 <h6><small>{idd.available ? <h3 className='text-green-500'>Available</h3> : <h3 className='text-red-600'>Not Available</h3>}</small></h6>
                 <div className="flex justify-around mt-[2vh]">
                   <Tooltip arrow title="Add User"><img src={add} alt="Add to team" className='h-[3vh] cursor-pointer'/></Tooltip>
-                  <Tooltip arrow title="Edit User"><img src={edit} alt="Edit User" className='h-[3vh] cursor-pointer'/></Tooltip>
+                  <Tooltip arrow title="Edit User"><img onClick={edituser} src={edit} alt="Edit User" className='h-[3vh] cursor-pointer'/></Tooltip>
                   <Tooltip arrow title="Delete User"><img src={del} alt="Delete User" className='h-[3vh] cursor-pointer'/></Tooltip>
                   
                   
