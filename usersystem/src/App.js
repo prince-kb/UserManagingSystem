66e852e1d7543831ba27f2f3b4f63de87710be95
Prince <princejaj67@gmail.com>
2024-04-09 10:43:16 +0530

    Improved loading part

--- usersystem/src/App.js
+++ usersystem/src/App.js
@@ -1,59 +1,81 @@
-import { useEffect, useState,useLayoutEffect } from "react";
+import { useEffect, useState} from "react";
 import UserItem from "./components/UserItem";
-import data from './assets/heliverse_mock_data.json'
 import la from './assets/svg/leftarrow.svg'
 import ra from './assets/svg/rightarrow.svg'
+import spinner from './assets/svg/spinner.svg'
+
+
 function App() {
-  const [fixedUsers,setFixedUsers]= useState(data);
-
-
-//   const p = async()=>{
-//     const response = await fetch(`${process.env.REACT_APP_HOST}/api/users`, {
-//     method: "GET",
-//     headers: {
-//       "Content-Type": "application/json",
-//     }
-//   });
-//   const n = await response.json();
-//   return n;
-// }
-
-
-  const [list,setList]=useState(fixedUsers);
+  const [fixedUsers,setFixedUsers]= useState([]);
+  const [isLoading,setLoading]=useState(true);
+  const [suser,setSuser]=useState()
+  const [list,setList]=useState([]);
+  const [l,setL] = useState([]);
   const [i,setI]=useState(0);
-  let l = list.slice(20*i,20*i+20<list.length ? 20*i+20 : list.length)
 
-  const [suser,setSuser]=useState({s : ""})
+
+  const p = async()=>{
+    const response = await fetch(`${process.env.REACT_APP_HOST}/api/users`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    }
+  });
+  const n = await response.json();
+  return n;
+}
+
+useEffect(()=>{
+  p().then((da)=>{
+    setFixedUsers(da.users)})
+},[])
+
+useEffect(()=>{
+  if(fixedUsers.length >0 ){
+    setList(fixedUsers)
+    setLoading(false);
+  }
+},[fixedUsers])
  
+useEffect(()=>{
+  setL(list.slice(20*i,20*i+20<list.length ? 20*i+20 : list.length));
+},[list,i])
+
+
   const onChange=(e)=>{
-    setSuser({...suser,[e.target.name] : e.target.value})
+    // console.log(e)
+    setSuser(e.target.value)
     let z = e.target.value.toLowerCase()
     setList(list.filter((val)=>{
       return val.first_name.slice(0,z.length).toLowerCase()===z || val.last_name.slice(0,z.length).toLowerCase()===z
     }))
   }
 
-    const submit=async (e)=>{
-    e.preventDefault();
+    const submit=(e)=>{
+    e.preventDefault()
     setI(0);
-    let z = suser.s.toLowerCase
+    setL(list.slice(20*i,20*i+20<list.length ? 20*i+20 : list.length));
+    let z = suser.toLowerCase
     setList(list.filter((val)=>{
       return val.first_name.slice(0,z.length).toLowerCase()===z|| val.last_name.slice(0,z.length).toLowerCase()===z
     }))
   }
+
     // setList(fixedUsers)}
 
   return (
     <div className=" text-center flex-col">
+      {/* <div><div className="tenor-gif-embed" data-postid="23918898" data-share-method="host" data-aspect-ratio="1" data-width="100%"><a href="https://tenor.com/view/loading-gif-gif-23918898">Loading Gif Sticker</a>from <a href="https://tenor.com/search/loading+gif-stickers">Loading Gif Stickers</a></div> <script type="text/javascript" async src="https://tenor.com/embed.js"></script></div> */}
       <h1 className="h1 container bg-emerald-300">Welcome</h1>
       <div className="mx-2 flex items-center justify-center gap-[2vw]">
-      <input type="text" onBlur={()=>setList(fixedUsers)} className="form-control" id="s" placeholder="Search user by name" name="s" value={suser.s} onChange={onChange} minLength={1}/>
+      <input type="text" onBlur={()=>setList(fixedUsers)} className="form-control" id="s" placeholder="Search user by name" name="s" value={suser} onChange={onChange} minLength={1}/>
         <button type="button" className="btn btn-primary" onClick={submit}>Search</button>
         </div>
 
       <div className="card card-body" style={{width: "100vw"}}>
             <div className="row">
                 <h2 className="h2 d-flex justify-content-center"> <b>All Users</b> </h2>
+                {isLoading && <div className="flex justify-center"><img src={spinner} alt="Loading....." /></div>}
                 {l.length>0 ? l.map((user,index) =>(
                   <UserItem key={index} user = {user}/>)
                  ) : <h2 className="h2">No Users available</h2>}
--- usersystem/src/redux/store.js
+++ usersystem/src/redux/store.js
@@ -1,9 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit'
-import user from './user/userSlice'
 import userdata from './user/dataSlice'
 export const store = configureStore({
   reducer: {
-    user : user,
     userdata : userdata
   },
 })
